2a85eb61ab020b1b3dd1f2f9c96c383bd6c2621b
Natália Doreto <dorettto@icloud.com>
2025-03-20 10:53:00 -0300

    Update raizes_delta.java

--- src/exercicios/raizes_delta.java
+++ src/exercicios/raizes_delta.java
@@ -5,21 +5,24 @@ public class Raizes {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner (System.in);
-		double a, b, c, delta, raizA, raizB;
+		double a, b, c, delta = 0, raizA = 0, raizB = 0;
 		a = entrada.nextInt();
 		b = entrada.nextInt();
 		c = entrada.nextInt();
 		
-		delta = b*b-(4*a*c);
-		raizA = (-b+Math.sqrt(delta))/2*a;
-		raizB = (-b-Math.sqrt(delta))/2*a;
-		
-		if (delta > 0) {
-			System.out.println("2 raizes reais e distintas: " + raizA +" e " +raizB);
-		} else if (delta == 0) {
-			System.out.println("2 raizes reais e iguais: " + raizA +" e " +raizB);
-		} else {
-			System.out.println("Não existem raizes reais: " + raizA +" e " +raizB);
+		if (a == 0 || b == 0 || c == 0) {
+			System.out.println("Nenhum coeficiente pode ser zero.");
+		}else {
+			delta = b*b-(4*a*c);
+			raizA = (-b+Math.sqrt(delta))/2*a;
+			raizB = (-b-Math.sqrt(delta))/2*a;
+				if (delta > 0) {
+					System.out.println("2 raizes reais e distintas: " + raizA +" e " +raizB);
+				} else if (delta == 0) {
+					System.out.println("2 raizes reais e iguais: " + raizA +" e " +raizB);
+				} else {
+					System.out.println("Não existem raizes reais: " + raizA +" e " +raizB);
+				}
 		}
 		
 		entrada.close();
